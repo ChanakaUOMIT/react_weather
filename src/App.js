b33877573760c46a2cb8ba9acc8bc2d93cd18b6e
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2019-02-07 13:36:50 +0530

    implemented

--- src/App.js
+++ src/App.js
@@ -1,28 +1,141 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Components/Weather/Weather";
+import Form from "./Components/Form/Form";
+import Titles from "./Components/Title/Title";
 
-class App extends Component {
+// const Api_Key = "8d2de98e089f1c28e1a22fc19a24ef04";
+// const Api_Key = "3472f85c5a93466aea8a40db53de8a9b";
+const Api_Key = "7d0d4a2ee1b3c8de186f4bf5da88a9da";
+
+const List=[
+  {
+    "CityCode":"1248991",
+    "CityName":"Colombo",
+    "Temp":"33.0",
+    "Status":"Clouds"
+  },
+  {
+    "CityCode":"1850147",
+    "CityName":"Tokyo",
+    "Temp":"8.6",
+    "Status":"Clear"
+  },
+  {
+    "CityCode":"2644210",
+    "CityName":"Liverpool",
+    "Temp":"16.5",
+    "Status":"Rain"
+  },
+  {
+    "CityCode":"2988507",
+    "CityName":"Paris",
+    "Temp":"22.4",
+    "Status":"Clear"
+  },
+  {
+    "CityCode":"2147714",
+    "CityName":"Sydney",
+    "Temp":"27.3",
+    "Status":"Rain"
+  },
+  {
+    "CityCode":"4930956",
+    "CityName":"Boston",
+    "Temp":"4.2",
+    "Status":"Mist"
+  },
+  {
+    "CityCode":"1796236",
+    "CityName":"Shanghai",
+    "Temp":"10.1",
+    "Status":"Clouds"
+  },
+  {
+    "CityCode":"3143244",
+    "CityName":"Oslo",
+    "Temp":"-3.9",
+    "Status":"Clear"
+  }
+]
+class App extends React.Component {
+
+  state = {
+
+    temperature: undefined,
+    // city: undefined,
+    name: undefined,
+    // humidity: undefined,
+    description: undefined,
+    id:undefined,
+    error: undefined
+  }
+
+  //getWeather is a method we'll use to make the api call
+  getWeather = async (e) => {
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();   
+    // const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_Key}`);
+
+    const response = await api_call.json();
+    console.log(response);
+    console.log("Description "+response.weather[0].description);
+    console.log(response.main.temp);
+    console.log(response.id);
+    console.log(response.name);
+
+    // if(city && country){
+    if(city){
+      this.setState({
+        temperature: response.main.temp,
+        name: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        id:response.id,
+        error: ""
+      })
+    }else{
+      this.setState({
+        error: "Please input search values..."
+      })
+    }
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div>
+         <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                <Form loadWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    name={this.state.name}
+                    // country={this.state.country}
+                    // humidity={this.state.humidity}
+                    id={this.state.id}
+                    description={this.state.description}
+                    error={this.state.error}
+                    data={List}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    );
+
+    )
   }
 }
-
 export default App;
